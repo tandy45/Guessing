40a9d0690ccb1f5f636a56fd2c150d1c2e09c97d
tandy45 <lewis_tandy@hotmail.com>
2019-06-11 08:56:32 +0100

    added index and js files

---
+++ guessing.js
@@ -0,0 +1,18 @@
+// create secret number
+
+var secretNumber = 4;
+
+//ask user for guess
+
+var guess = prompt('Guess a number');
+alert("You guessed: " + guess);
+
+//check if guess is correct
+
+if(parseInt(guess) === secretNumber) {
+    alert("You got it right");
+} else if(parseInt(guess) > secretNumber){
+    alert("your guess is too high.");
+} else if(parseInt(guess) === secretNumber) {
+    alert("Your guess is too low");
+}
